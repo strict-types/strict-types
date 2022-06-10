2e5267bd143aa568f43a4a123994859381986832
Dr Maxim Orlovsky <orlovsky@pandoraprime.ch>
2022-06-10 11:56:52 +0200

    Repo template

---
+++ README.md
@@ -0,0 +1,7 @@
+# Stens: rust implementation of strict encoding schema library
+
+Strict encoding is a binary serialization format for Internet, web, AluVM & 
+LNP/BP stack. This library provides primitives for describing strict encoding
+schemata, validating and parsing structured data using the schema.
+
+To learn more about strict encoding [read the blueprint](https://sten.lnp-bp.org).
---
+++ src/lib.rs
@@ -0,0 +1,11 @@
+// Strict encoding schema library, implementing validation and parsing of strict
+// encoded data against the schema.
+//
+// Written in 2022 by
+//     Dr. Maxim Orlovsky <orlovsky@lnp-bp.org>
+//
+// Copyright (C) 2022 by LNP/BP Standards Association, Switzerland.
+//
+// You should have received a copy of the MIT License along with this software.
+// If not, see <https://opensource.org/licenses/MIT>.
+
