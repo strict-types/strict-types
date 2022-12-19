fa4217a1d295fe89172114d4c68bd326d6fdc1e1
Dr. Maxim Orlovsky <orlovsky@lnp-bp.org>
2022-12-19 13:10:24 +0100

    Improve license plate

--- src/ast/translate.rs
+++ src/ast/translate.rs
@@ -1,5 +1,5 @@
-// Strict encoding schema library, implementing validation and parsing of strict
-// encoded data against the schema.
+// Strict encoding schema library, implementing validation and parsing
+// strict encoded data against a schema.
 //
 // SPDX-License-Identifier: Apache-2.0
 //
