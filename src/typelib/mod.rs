66209fb659cdcde1034c7ccb11b9fd8f439c19d4
Dr Maxim Orlovsky <orlovsky@ubideco.org>
2023-05-25 20:11:35 +0200

    typelib: re-export parse_args

--- src/typelib/mod.rs
+++ src/typelib/mod.rs
@@ -39,3 +39,6 @@ pub use translate::SymbolError;
 pub use transpile::LibBuilder;
 pub(crate) use type_lib::TypeMap;
 pub use type_lib::{Dependency, ExternRef, InlineRef, InlineRef1, InlineRef2, LibRef, TypeLib};
+
+#[deprecated(since = "1.3.0", note = "import from the crate root")]
+pub use super::parse_args;
